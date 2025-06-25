aa275199723f28ac3dfc4379ef0fdac94f255749
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-25 14:09:45 +0200

    Aggiungi il file hobbies.js e implementa la funzione hobbyArray in main.js

---
+++ assets/hobbies.js
@@ -0,0 +1,12 @@
+// - hobbies.js
+// Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+// Esporta la funzione dal file.
+
+function hobbyArray(hobbyOne, hobbyTwo, hobbyThree) {
+  const hobbies = [];
+  hobbies.push(hobbyOne, hobbyTwo, hobbyThree);
+
+  return hobbies;
+}
+
+module.exports = hobbyArray;
--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,9 @@
 const fullName = require("./name");
 
+const hobbies = require("./hobbies");
+
 const name = "Davide";
 const surname = "Criscuolo";
 console.log(fullName(name, surname));
+
+console.log(hobbies("Uscire", "Palestra", "Games"));
