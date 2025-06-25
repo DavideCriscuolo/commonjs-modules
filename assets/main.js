1d9cf741c7d1fcf15e11a1975418083702fbab23
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-25 14:31:54 +0200

    Aggiungi il file people.js e implementa la funzione buildPerson in main.js

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,11 @@
 const fullName = require("./name");
 
 const hobbies = require("./hobbies");
+const buildPerson = require("./people.js");
 
 const name = "Davide";
 const surname = "Criscuolo";
 console.log(fullName(name, surname));
-
 console.log(hobbies("Uscire", "Palestra", "Games"));
+
+buildPerson();
